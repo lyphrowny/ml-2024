2c3e7af62a25758b2717d96b1b426915d47fa949
lyphrowny <strukov.as@edu.spbstu.ru>
2025-04-14 15:47:53 +0300

    Add result table

--- team-12/main.py
+++ team-12/main.py
@@ -24,6 +24,8 @@ from PIL import Image
 import pandas as pd
 import reportlab.lib.pagesizes as pagesizes
 from reportlab.pdfgen import canvas
+from itertools import chain
+from collections import defaultdict
 
 from ocr import ocr_image
 from race_result import RaceResult
@@ -177,19 +179,109 @@ class MainWindow(QMainWindow):
         self.class_combo.clear()
         self.class_combo.addItems([row[0] for row in cursor.fetchall()])
 
-    def build_table_result(self): ...
+    def build_table_result(self):
+        cursor = self.conn.cursor()
+
+        cursor.execute(
+            "SELECT id, date_added FROM races WHERE class_name = (?)",
+            (self.class_combo.currentText(),),
+        )
+        races = cursor.fetchall()
+        race_ids, race_dates = list(zip(*races))
+        print(races)
+        print(race_dates)
+        race_dates = [
+            datetime.strptime(race_date, "%Y-%m-%d").strftime("%d.%m.%Y")
+            for race_date in race_dates
+        ]
+        rid_to_tcol = dict(zip(race_ids, range(1, 1 + len(race_ids))))
+        print(f"{rid_to_tcol = }")
+
+        self.results_table.setColumnCount(2 + len(race_ids))
+        self.results_table.setHorizontalHeaderLabels(
+            [
+                "Участник",
+                *race_dates,
+                "Итог",
+            ]
+        )
+
+        placeholders = lambda what: f"({', '.join('?' for _ in what)})"
+        print(race_ids)
+
+        cursor.execute(
+            f"SELECT participant_id, race_id, position FROM race_results WHERE race_id IN {placeholders(race_ids)}",
+            (*race_ids,),
+        )
+        data = cursor.fetchall()
+        p_ids, race_ids, positions = list(zip(*data))
+        pid_to_data = defaultdict(list)
+        for p_id, race_id, pos in data:
+            pid_to_data[p_id].append((race_id, pos))
+        # pid_to_data = dict(zip(p_ids, zip(race_ids, positions)))
+        print(pid_to_data)
+        p_ids = tuple(pid_to_data.keys())
+        # print(len(p_ids), len(race_ids), len(positions))
+
+        cursor.execute(
+            f"SELECT DISTINCT id, full_name FROM participants WHERE id IN {placeholders(p_ids)}",
+            (*p_ids,),
+        )
+        pid_to_name = dict(cursor.fetchall())
+        # print(p_ids, positions)
+        self.results_table.setRowCount(len(set(p_ids)))
+
+        rating = [
+            250,
+            210,
+            180,
+            155,
+            135,
+            120,
+            110,
+            100,
+            82,
+            78,
+            74,
+            70,
+            66,
+            62,
+            58,
+            54,
+            50,
+            47,
+            44,
+            41,
+            38,
+            35,
+            32,
+            29,
+        ]
+        rating.extend(range(26, 0, -1))
+
+        for i, p_id in enumerate(pid_to_data, start=1):
+            self.results_table.setItem(
+                i - 1, 0, QTableWidgetItem(str(pid_to_name[p_id]))
+            )
+            print(pid_to_data[p_id])
+            for race_id, pos in pid_to_data[p_id]:
+                self.results_table.setItem(
+                    i - 1, rid_to_tcol[race_id], QTableWidgetItem(str(rating[pos]))
+                )
+
+        self.results_table.resizeColumnsToContents()
 
     def _on_image_loaded(self):
         race_typ, date, participants = self.image_loader_dialog.export_table()
         self.image_loader_dialog.close()
 
         cursor = self.conn.cursor()
-        print(f"{race_typ = }")
-        print(f"{date = }")
+        # print(f"{race_typ = }")
+        # print(f"{date = }")
 
-        cursor.execute("select * from races")
-        res = cursor.fetchall()
-        print(res)
+        # cursor.execute("select * from races")
+        # res = cursor.fetchall()
+        # print(res)
 
         # Insert or get race ID
         cursor.execute(
@@ -207,7 +299,7 @@ class MainWindow(QMainWindow):
             ),
         )
         race_id = cursor.fetchone()[0]
-        print(race_id)
+        # print(race_id)
 
         for pos, participant in enumerate(participants, start=1):
             # Insert or get participant
@@ -220,8 +312,9 @@ class MainWindow(QMainWindow):
                 (participant,),
             )
             participant_id = cursor.fetchone()[0]
-            print(f"{participant_id = }")
+            # print(f"{participant_id = }")
 
+            print(race_id, f"{pos = }", participant, f"{participant_id = }")
             # Insert race result
             cursor.execute(
                 """
@@ -231,11 +324,24 @@ class MainWindow(QMainWindow):
                 DO UPDATE SET
                     race_id = excluded.race_id,
                     participant_id = excluded.participant_id,
-                    position = position
+                    position = excluded.position
             """,
-                (race_id, participant_id, pos),
+                (
+                    race_id,
+                    participant_id,
+                    pos,
+                ),
             )
         self.conn.commit()
+
+        cursor.execute(
+            "SELECT race_id, position, participant_id FROM race_results WHERE race_id = (?)",
+            (race_id,),
+        )
+        print(cursor.fetchall())
+
+        # update the combobox items
+        self.populate_classes()
 
 
 class ImageLoaderDialog(QDialog):
